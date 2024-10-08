ab7ca1b4abe5946a496f019e3b8c245646ca1779
Paulina Macdid <paulinamacdid@hotmail.co.uk>
2024-10-08 15:18:10 +0100

    added new columns to users model

--- app/models/sheep.rb
+++ app/models/sheep.rb
@@ -1,3 +1,6 @@
 class Sheep < ApplicationRecord
+  has_many :bookings
   belongs_to :user
+  validates :name, :image_url, :location, :breed, :weight, presence: true
+  validates :name, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_08_114513) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_08_140115) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.float "price_total"
+    t.bigint "sheep_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["sheep_id"], name: "index_bookings_on_sheep_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "sheep", force: :cascade do |t|
+    t.string "name"
+    t.string "image_url"
+    t.string "location"
+    t.text "description"
+    t.float "weight"
+    t.string "breed"
+    t.float "day_rate"
+    t.integer "rating"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sheep_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,7 +49,15 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_08_114513) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.text "picture"
+    t.string "location"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "bookings", "sheep"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "sheep", "users"
 end
