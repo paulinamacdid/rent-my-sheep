8801d9091c6dc28bf42cbe704f913d986e4ced59
Dorothea Toeller <toellerdorothea@gmail.com>
2024-10-08 13:59:53 +0100

    create sheep and booking model and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,50 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 0) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_08_120915) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.float "price_total"
+    t.bigint "sheep_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["sheep_id"], name: "index_bookings_on_sheep_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "sheep", force: :cascade do |t|
+    t.string "name"
+    t.string "image_url"
+    t.string "location"
+    t.text "description"
+    t.float "weight"
+    t.string "breed"
+    t.float "day_rate"
+    t.integer "rating"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sheep_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "bookings", "sheep"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "sheep", "users"
 end
