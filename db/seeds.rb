b41584882345278a42907411db7a7e583b7d7471
Katherine Heard <katherinebeheard@gmail.com>
2024-10-09 11:52:31 +0100

    finished the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,21 +21,23 @@ george = User.create(email: "georgyporgy@gmail.com", password: "asdisdf3!", firs
 
 puts "creating sheep...."
 
-Sheep.create(name: "Shaun The Sheep", image_url: "Shaun.png", location: "Hollywood", description: "Shaun made his
+Sheep.create(name: "Shaun The Sheep", image_url: "https://media.timeout.com/images/102765296/1372/772/image.webp", location: "Hollywood", description: "Shaun made his
   screen debut in the Wallace and Gromit film A Close Shave back in 1995. Fast-forward to now and Shaun has become
-  a global superstar with 5 million followers on Facebook! There is even a Shaun-themed café in Japan.", weight:
-  "140kg", breed: "Aberblack", day_rate: "450.0", rating: "10", user: gabriela)
+  a global superstar with 5 million followers on Facebook! There is even a Shaun-themed café in Japan. If you want
+  to be close to a Hollywood superstar Shaun is your sheep!", weight: "140kg", breed: "Aberblack", day_rate: "450.0", rating: "10", user: gabriela)
 
-Sheep.create(name: "Baa Baa Black Sheep", image_url: "/images/baa_baa.png", location: "Cardiff", description: "Baa Baa is
-  named after the medieval English great or old Custom wool tax of 1275, which survived until the fifteenth century.",
+Sheep.create(name: "Baa Baa Black Sheep", image_url: "https://www.nativebreed.org/wp-content/uploads/2021/06/Balwen-Welsh.jpeg", location: "Cardiff", description: "Baa Baa is
+  named after the medieval English great or old Custom wool tax of 1275, which survived until the fifteenth century. Baa Baa
+  is a classic sheep we all know and love and he loves it when people sing Baa Baa Black Sheep",
   weight: "90kg", breed: "Balwen Welsh Mountain", day_rate: "153.50", rating: "7", user: pamela)
 
-Sheep.create(name: "Dolly", image_url: "dolly.png", location: "London", description: "Dolly the sheep was the first cloned mammal
+Sheep.create(name: "Dolly", image_url: "https://source.colostate.edu/wp-content/uploads/2017/02/DollySideView-e1487353921899.jpg", location: "London", description: "Dolly the sheep was the first cloned mammal
   ever to be created from an adult cell. Affectionately named after country music legend Dolly Parton,
-  Dolly caused excitement and controversy among both scientists and the public.", weight:
+  Dolly caused excitement and controversy among both scientists and the public.  If you fancy meeting
+  the ultimate contraversial sheep Dolly is the one but not the only", weight:
   "60kg", breed: "Finn Dorset sheep", day_rate: "287.0", rating: "8", user: david)
 
-Sheep.create(name: "Daisy", image_url: "french_sheep.png", location: "Paris", description: "Daisy is a fun-lovin' sheep who loves
+Sheep.create(name: "Daisy", image_url: "https://www.berrichonsociety.com/wp-content/uploads/2022/03/Frontsolo-1500x630.jpg", location: "Paris", description: "Daisy is a fun-lovin' sheep who loves
   to give love! She is great for cuddles and enjoys having her wool sheered for the winter to make way for better
   dance moves at the summer sheep raves!", weight: "80kg", breed: "Berrichon du Cher", day_rate: "300.0", rating:
   "4", user: george)
