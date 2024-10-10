8ae7e5012f2d85ff2db3daf93379dce5a5572684
Dorothea Toeller <toellerdorothea@gmail.com>
2024-10-10 17:13:08 +0100

    make cloudinary work

--- app/models/sheep.rb
+++ app/models/sheep.rb
@@ -1,6 +1,7 @@
 class Sheep < ApplicationRecord
   has_many :bookings
   belongs_to :user
-  validates :name, :image_url, :location, :breed, :weight, presence: true
+  validates :name, :location, :breed, :weight, presence: true
   validates :name, uniqueness: true
+  has_one_attached :photo
 end
